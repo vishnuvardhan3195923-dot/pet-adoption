514f5bcbd9135a48529f6baedc27152d6a53dd00
vishnuvardhan3195923-dot <vishnuvardhan3195923@gmail.com>
2026-03-28 22:57:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,11 +16,48 @@ card.innerHTML = `
 </div>
 `;
 
-card.onclick = () => {
-localStorage.setItem("selectedPet", JSON.stringify(pet));
-window.location.href = "pet-details.html";
+card.onclick = function() {
+goToDetails(card, p);
 };
 
 container.appendChild(card);
 });
 });
+function goToDetails(card, pet){
+
+// store selected pet
+localStorage.setItem("selectedPet", JSON.stringify(pet));
+
+// clone card
+let clone = card.cloneNode(true);
+let rect = card.getBoundingClientRect();
+
+clone.style.position = "fixed";
+clone.style.top = rect.top + "px";
+clone.style.left = rect.left + "px";
+clone.style.width = rect.width + "px";
+clone.style.zIndex = "9999";
+clone.style.transition = "all 0.5s ease";
+
+document.body.appendChild(clone);
+
+// hide original
+card.style.opacity = "0";
+
+// animate
+setTimeout(()=>{
+clone.style.top = "50%";
+clone.style.left = "50%";
+clone.style.transform = "translate(-50%, -50%) scale(1.2)";
+},10);
+
+// fade page
+document.body.style.transition = "0.4s";
+document.body.style.opacity = "0.7";
+
+// go next page
+setTimeout(()=>{
+window.location.href = "pet-details.html";
+},450);
+
+}
