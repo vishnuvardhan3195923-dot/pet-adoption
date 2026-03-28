af729efeea73e275d4ff936d36859bf5d96e7506
vishnuvardhan3195923-dot <vishnuvardhan3195923@gmail.com>
2026-03-28 23:13:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
 fetch('pets.json')
 .then(res => res.json())
 .then(data => {
+
 const container = document.getElementById("pets-container");
 
 data.forEach(pet => {
+
 const card = document.createElement("div");
 card.classList.add("card");
 
@@ -17,42 +21,21 @@ card.innerHTML = `
 `;
 
 card.onclick = function() {
-goToDetails(card, pet); // ✅ FIXED HERE
+goToDetails(card, pet);
 };
 
 container.appendChild(card);
+
 });
+
+});
+
 });
 
 function goToDetails(card, pet){
 
 localStorage.setItem("selectedPet", JSON.stringify(pet));
 
-let clone = card.cloneNode(true);
-let rect = card.getBoundingClientRect();
-
-clone.style.position = "fixed";
-clone.style.top = rect.top + "px";
-clone.style.left = rect.left + "px";
-clone.style.width = rect.width + "px";
-clone.style.zIndex = "9999";
-clone.style.transition = "all 0.5s ease";
-
-document.body.appendChild(clone);
-
-card.style.opacity = "0";
-
-setTimeout(()=>{
-clone.style.top = "50%";
-clone.style.left = "50%";
-clone.style.transform = "translate(-50%, -50%) scale(1.2)";
-},10);
-
-document.body.style.transition = "0.4s";
-document.body.style.opacity = "0.7";
-
-setTimeout(()=>{
-window.location.href = "pet-details.html";
-},450);
+window.location.href = "pet-details.html"; // simplify first
 
 }
