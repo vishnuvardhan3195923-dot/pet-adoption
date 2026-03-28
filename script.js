c2ba205fcafbbe58838f5f10e720d6dcd21b1ae4
vishnuvardhan3195923-dot <vishnuvardhan3195923@gmail.com>
2026-03-28 19:03:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 fetch('pets.json')
 .then(res => res.json())
 .then(data => {
-    const container = document.getElementById("pets-container");
+const container = document.getElementById("pets-container");
 
-    data.forEach(pet => {
-        const card = document.createElement("div");
-        card.classList.add("card");
+data.forEach(pet => {
+const card = document.createElement("div");
+card.classList.add("card");
 
-        card.innerHTML = `
-            <img src="${pet.image}">
-            <div class="card-content">
-                <h3>${pet.name}</h3>
-                <p>${pet.breed}</p>
-                <p>${pet.age}</p>
-            </div>
-        `;
+card.innerHTML = `
+<img src="${pet.image}">
+<div class="card-content">
+<h3>${pet.name}</h3>
+<p>${pet.breed}</p>
+<p>${pet.age}</p>
+</div>
+`;
 
-        card.onclick = () => {
-            localStorage.setItem("selectedPet", JSON.stringify(pet));
-            window.location.href = "pet-details.html";
-        };
+card.onclick = () => {
+localStorage.setItem("selectedPet", JSON.stringify(pet));
+window.location.href = "pet-details.html";
+};
 
-        container.appendChild(card);
-    });
+container.appendChild(card);
+});
 });
