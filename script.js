55497166a0ffd850b5e128d33d9920f509d0b5db
vishnuvardhan3195923-dot <vishnuvardhan3195923@gmail.com>
2026-03-28 17:03:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-fetch('./pets.json')
-.then(response => response.json())
+fetch('pets.json')
+.then(res => res.json())
 .then(data => {
-    let container = document.getElementById("pets-container");
+    const container = document.getElementById("pets-container");
 
     data.forEach(pet => {
-        let card = document.createElement("div");
+        const card = document.createElement("div");
         card.classList.add("card");
 
         card.innerHTML = `
-            <img src="${pet.image}" width="100%">
-            <h3>${pet.name}</h3>
-            <p>Breed: ${pet.breed}</p>
-            <p>Age: ${pet.age}</p>
+            <img src="${pet.image}">
+            <div class="card-content">
+                <h3>${pet.name}</h3>
+                <p>${pet.breed}</p>
+                <p>${pet.age}</p>
+            </div>
         `;
 
         container.appendChild(card);
     });
-})
-.catch(error => console.error("Error loading JSON:", error));
+});
