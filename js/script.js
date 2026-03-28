4189f7b050f356e125c5ef75576d2a18d3f413cc
vishnuvardhan3195923-dot <vishnuvardhan3195923@gmail.com>
2026-03-28 16:25:15 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+fetch('pets.json')
+.then(response => response.json())
+.then(data => {
+    let container = document.getElementById("pets-container");
+
+    data.forEach(pet => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <img src="${pet.image}" width="100%">
+            <h3>${pet.name}</h3>
+            <p>Breed: ${pet.breed}</p>
+            <p>Age: ${pet.age}</p>
+        `;
+
+        container.appendChild(card);
+    });
+});
